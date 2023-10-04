edc27631a699cfda9f468bb495a9097c66005249
nalbam <me@nalbam.com>
2023-10-04 17:33:22 +0000

    add timer

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Leaderboard from './pages/Leaderboard'
 import Manage from './pages/Manage'
 import ManageLeague from './pages/ManageLeague'
 import ManageRacer from './pages/ManageRacer'
+import Timer from './pages/Timer'
 
 import { Amplify } from 'aws-amplify';
 
@@ -39,6 +40,7 @@ const routing = (
         <Route path='/manage/' element={<Manage />} />
         <Route path='/racers/:league' element={<Leaderboard />} />
         <Route path='/league/:league' element={<Leaderboard />} />
+        <Route path='/timer' element={<Timer />} />
         <Route element={App} />
       </Routes>
     </BrowserRouter>
